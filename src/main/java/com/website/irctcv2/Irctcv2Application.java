488bdffee348e6052212f9aab4f4b85bf8275f05
iDK-pseudo <shivamsharma151999@gmail.com>
2023-02-08 22:38:50 +0530

    Initial Commit

---
+++ src/main/java/com/website/irctcv2/Irctcv2Application.java
@@ -0,0 +1,13 @@
+package com.website.irctcv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Irctcv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Irctcv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/website/irctcv2/Irctcv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.irctcv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Irctcv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
